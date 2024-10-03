dd39fd2967e272ebbb74fefa1aa26612fbb45c74
dvaldez <dvaldez@no-reply.com>
2024-10-02 23:25:22 -0600

    Auto-generated commit

--- main.js
+++ main.js
@@ -43619,22 +43619,23 @@ var TooltipDirective = class _TooltipDirective {
   // Método para crear el tooltip
   createTooltip() {
     this.tooltipElement = this.renderer.createElement("span");
-    this.renderer.appendChild(
-      this.tooltipElement,
-      this.renderer.createText(this.tooltipText)
-      // Añadimos el texto del tooltip
-    );
+    this.renderer.appendChild(this.tooltipElement, this.renderer.createText(this.tooltipText));
     this.renderer.setStyle(this.tooltipElement, "position", "absolute");
     this.renderer.setStyle(this.tooltipElement, "background-color", "#333");
     this.renderer.setStyle(this.tooltipElement, "color", "#fff");
     this.renderer.setStyle(this.tooltipElement, "padding", "5px 10px");
     this.renderer.setStyle(this.tooltipElement, "border-radius", "4px");
-    this.renderer.setStyle(this.tooltipElement, "top", `${this.el.nativeElement.offsetTop - 30}px`);
-    this.renderer.setStyle(this.tooltipElement, "left", `${this.el.nativeElement.offsetLeft}px`);
     this.renderer.setStyle(this.tooltipElement, "font-size", "12px");
     this.renderer.setStyle(this.tooltipElement, "white-space", "nowrap");
     this.renderer.setStyle(this.tooltipElement, "z-index", "1000");
-    this.renderer.appendChild(document.body, this.tooltipElement);
+    const hostPos = this.el.nativeElement.getBoundingClientRect();
+    if (this.tooltipElement) {
+      const tooltipPosTop = hostPos.top - this.tooltipElement.offsetHeight - 20;
+      const tooltipPosLeft = hostPos.left - 5;
+      this.renderer.setStyle(this.tooltipElement, "top", `${tooltipPosTop}px`);
+      this.renderer.setStyle(this.tooltipElement, "left", `${tooltipPosLeft}px`);
+      this.renderer.appendChild(document.body, this.tooltipElement);
+    }
   }
   // Método para eliminar el tooltip
   destroyTooltip() {
@@ -43756,7 +43757,7 @@ var DashboardHeroDetailsComponent = class _DashboardHeroDetailsComponent {
   static \u0275fac = function DashboardHeroDetailsComponent_Factory(__ngFactoryType__) {
     return new (__ngFactoryType__ || _DashboardHeroDetailsComponent)(\u0275\u0275directiveInject(HeroService));
   };
-  static \u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _DashboardHeroDetailsComponent, selectors: [["app-dashboard-hero-details"]], inputs: { renderHeroDetail: "renderHeroDetail" }, standalone: true, features: [\u0275\u0275NgOnChangesFeature, \u0275\u0275StandaloneFeature], decls: 1, vars: 1, consts: [["class", "dashboard-hero-detail", "style", "display: flex; justify-content: space-evenly;position: relative;", 4, "ngIf"], [1, "dashboard-hero-detail", 2, "display", "flex", "justify-content", "space-evenly", "position", "relative"], [2, "font-weight", "bolder"], [1, "action"], ["appTooltip", "Editar", 3, "routerLink"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 0 512 512"], ["d", "M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"], ["appTooltip", "'Eliminar'", 2, "background-color", "tomato", "color", "#fff"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 0 448 512"], ["d", "M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"], [3, "isLoading", "message"]], template: function DashboardHeroDetailsComponent_Template(rf, ctx) {
+  static \u0275cmp = /* @__PURE__ */ \u0275\u0275defineComponent({ type: _DashboardHeroDetailsComponent, selectors: [["app-dashboard-hero-details"]], inputs: { renderHeroDetail: "renderHeroDetail" }, standalone: true, features: [\u0275\u0275NgOnChangesFeature, \u0275\u0275StandaloneFeature], decls: 1, vars: 1, consts: [["class", "dashboard-hero-detail", "style", "display: flex; justify-content: space-evenly; position: relative;", 4, "ngIf"], [1, "dashboard-hero-detail", 2, "display", "flex", "justify-content", "space-evenly", "position", "relative"], [2, "font-weight", "bolder"], [1, "action"], ["appTooltip", "Editar", 3, "routerLink"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 0 512 512"], ["d", "M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1 0 32c0 8.8 7.2 16 16 16l32 0zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"], ["appTooltip", "Eliminar", 2, "background-color", "tomato", "color", "#fff", "position", "relative"], ["xmlns", "http://www.w3.org/2000/svg", "viewBox", "0 0 448 512"], ["d", "M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"], [3, "isLoading", "message"]], template: function DashboardHeroDetailsComponent_Template(rf, ctx) {
     if (rf & 1) {
       \u0275\u0275template(0, DashboardHeroDetailsComponent_div_0_Template, 22, 7, "div", 0);
     }
